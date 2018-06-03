5d82c6e68978880a60ed41f426440011efad8003
feerliliso <feerliliso@hotmail.com>
2018-06-03 10:10:50 +0800

    add a file
    rt

---
+++ exercise.py
@@ -0,0 +1,27 @@
+a = "hello"
+print(a)
+
+b = "let`go"
+print(b)
+
+c = ' "The Zen of Python" -- by Tim Peters'
+
+print(c)
+
+a = "xyz"
+b = "abc"
+c = a+b
+d = "x"
+print(a)
+print(b)
+print(c)
+print(a[1]+a[2])
+list(filter(lambda x: x = d , b))
+
+
+
+
+
+
+
+
