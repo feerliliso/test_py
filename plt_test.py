444200173c0548d601569f3217776a029cccf7c3
feerliliso <feerliliso@hotmail.com>
2018-06-23 14:42:55 +0800

    add new file
    rt

---
+++ plt_test.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+# plt.plot([1, 3, 5], [4, 8, 10])
+# plt.show()
+import numpy as np
+# x = np.linspace(-np.pi, np.pi, 100)
+# #plt.plot(x, np.sin(x))
+# #plt.show()
+# plt.figure(1, dpi=50)
+# for i in range(10):
+#     plt.plot(x,np.sin(x/i))
+# plt.show()
+# plt.figure(1, dpi=50)
+# data = [2, 3, 4, 6, 3, 1, 3,]
+# plt.hist(data)
+# plt.show()
+# x = np.arange(1, 10)
+# y = x
+# plt.figure(1, dpi=50)
+# plt.scatter(x, y, c='r', marker='o')
+# plt.show()
